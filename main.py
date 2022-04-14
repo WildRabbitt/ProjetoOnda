f8f82847e1c09507c0bba8e7ef2b4c8a0c26c9d9
Thiago(OndaÁgil) <coelho.thiago189@gmail.com>
2022-04-14 17:55:50 -0400

    TESTE 1.2

--- main.py
+++ main.py
@@ -31,14 +31,13 @@ sleep(2)
 
 # Preencher assunto e setor
 navegador.find_element(By.XPATH, '//*[@id="1_grid"]/div/div[3]/table/tbody/tr/td[1]/div[4]/input[1]').send_keys('gin')
-navegador.find_element(By.XPATH, '//*[@id="1_grid"]/div/div[3]/table/tbody/tr/td[1]/div[4]/input[2]').send_keys(
-    'estoque')
+navegador.find_element(By.XPATH, '//*[@id="1_grid"]/div/div[3]/table/tbody/tr/td[1]/div[4]/input[2]').send_keys('estoque')
 sleep(1)
 navegador.find_element(By.XPATH, '/html/body/div[2]/div/div[3]/table/tbody/tr/td[5]/div[1]/input[1]').click()
 sleep(1.5)
 
 while row != 0:
-    try:
+
         xpathlog = '/html/body/div[2]/div/div[6]/table/tbody/tr[' + row + ']/td[1]/div'
 
         navegador.find_element(By.XPATH, xpathlog).click()  # Clicar
@@ -49,8 +48,6 @@ while row != 0:
 
         navegador.find_element(By.XPATH,'/html/body/form[2]/div[3]/div[1]/dl[6]/dd/button[2]').click()  # Abrir cadastro cliente
         sleep(1.5)
-        nome = navegador.find_element(By.ID, 'razao')  # Copiar razao social/nome cliente
-        print(nome)
         navegador.find_element(By.XPATH, '/html/body/form[3]/div[3]/ul/li[7]/a').click()  # Abrir aba contrato
         sleep(1)
 
@@ -58,8 +55,9 @@ while row != 0:
         xpathcont_stats = '/html/body/form[3]/div[3]/div[7]/dl/div/div/div[5]/table/tbody/tr[' + rowcont + ']/td[3]/div/span'
         statuscontr = navegador.find_element(By.XPATH, xpathcont_stats)  # Copiar o status do contrato
 
-        if statuscontr != ' (P) Pré-contrato':  # Verificar se o status está como pre cadastro
+        if statuscontr != ' (P) Pré-contrato':  # Verificar se o status não está como pre cadastro
             while statuscontr != " (P) Pré-contrato":
+                print (statuscontr)
                 rowcont = str(int(rowcont) + 1)
                 xpathcont_stats = '/html/body/form[3]/div[3]/div[7]/dl/div/div/div[5]/table/tbody/tr[' + rowcont + ']/td[3]/div/span'
                 statuscontr = navegador.find_element(By.XPATH, xpathcont_stats)
@@ -68,14 +66,13 @@ while row != 0:
 
         xpathcont_id = '/html/body/form[3]/div[3]/div[7]/dl/div/div/div[5]/table/tbody/tr[' + rowcont + ']/td[1]/div/span'
         idcont = navegador.find_element(By.XPATH, xpathcont_id)  # Pegar o id do contrato
-        print(idcont)
+        print("Numero contrato é"+idcont)
 
         navegador.find_element(By.XPATH,xpathcont_id).click()  #Selecionar o contrato
         navegador.find_element(By.NAME, 'editar').click()
 
         login = gerar_login(nome, idcont)
+        print(login)
 
-    except:
-        row = 0
 
 # pesquisa ID contrato
